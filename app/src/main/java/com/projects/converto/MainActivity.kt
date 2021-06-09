2ac3fcf3e7c25d7fb9494f827f0656e4b486dade
SAMYAK99 <singhsamyak199@gmail.com>
2021-06-09 23:33:32 +0530

    View Binding and Dagger Hilt Setup

--- app/src/main/java/com/projects/converto/MainActivity.kt
+++ app/src/main/java/com/projects/converto/MainActivity.kt
@@ -3,20 +3,24 @@ package com.projects.converto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.projects.converto.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
 
+    private  lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Setting Up View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
     }
-
-
-
 
 
 }
